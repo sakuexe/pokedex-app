8712f20502d4394a8a285592497fc872c7228dd2
sakuexe <saku.karttunen@gmail.com>
2023-07-18 11:46:00 +0300

    added package for pokeapi typing

--- app/pokelist/pokeitem.tsx
+++ app/pokelist/pokeitem.tsx
@@ -1,4 +1,4 @@
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import { Image } from "expo-image";
 // constants
 import { COLORS, IMAGES } from "@/constants";
@@ -6,6 +6,8 @@ import styles from "@/styles/common";
 // util functions
 import { capitalize } from "@/utils/string";
 import useFetch from "@/hooks/useFetch";
+// types
+import { PokeAPI } from "pokeapi-types";
 
 const API_URL = "https://pokeapi.co/api/v2/pokemon";
 
@@ -14,31 +16,14 @@ export type PokemonType = {
   url: string;
 };
 
-type PokemonData = {
-  abilities: any[];
-  base_experience: number;
-  forms: any[];
-  game_indices: any[];
-  height: number;
-  held_items: any[];
-  id: number;
-  is_default: boolean;
-  location_area_encounters: string;
-  moves: any[];
-  name: string;
-  order: number;
-  past_types: any[];
-  species: any[];
-  sprites: any[];
-  stats: any[];
-  types: any[];
-  weight: number;
-};
-
 export default function PokeItem(pokemon: PokemonType) {
-  const { data, isLoading, error } = useFetch<PokemonData>(
+  const { data, isLoading, error } = useFetch<PokeAPI.Pokemon>(
     `${API_URL}/${pokemon.name}`,
-  );
+  ) as {
+    data: PokeAPI.Pokemon | null;
+    isLoading: boolean;
+    error: Error | null;
+  };
 
   return (
     <TouchableOpacity style={styles.listing}>
@@ -52,7 +37,7 @@ export default function PokeItem(pokemon: PokemonType) {
       ) : (
         <>
           <Image
-            source={{ uri: data?.sprites["front_default"] }}
+            source={{ uri: data?.sprites.front_default }}
             placeholder={IMAGES.loading}
             style={styles.listingImage}
           />
